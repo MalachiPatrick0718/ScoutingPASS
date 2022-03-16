203e87ee22473d640ecddbd111ca8863e4f64f42
MalachiPatrick0718 <95603368+MalachiPatrick0718@users.noreply.github.com>
2022-03-15 23:31:47 -0400

    Update RR_config.js

--- 2022/RR_config.js
+++ 2022/RR_config.js
@@ -14,7 +14,7 @@ var config_data = `
       "Event":{
         "code":"e",
         "type":"event",
-        "defaultValue":"2022flwp",
+        "defaultValue":"NYLI2",
         "required":"true",
         "disabled":"true"
       },
